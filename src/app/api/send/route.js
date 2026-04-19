af9c6ad8aee70f71565a629419a06d055731941b
MilanDorfling <154674320+MilanDorfling@users.noreply.github.com>
2026-04-19 17:03:34 +0200

    fixed routes api for next.js 16+

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,15 +1,20 @@
 import { Resend } from "resend";
 import { NextResponse } from "next/server";
 
-// ~~ Email sending logic using Resend API ~~
-
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req) {
   const { name, email, message } = await req.json();
 
+  if (!process.env.RESEND_API_KEY) {
+    return NextResponse.json(
+      { error: "Missing RESEND_API_KEY" },
+      { status: 500 },
+    );
+  }
+
   const { data, error } = await resend.emails.send({
-    from: ` ${name} from milandorfling.dev<onboarding@resend.dev>`,
+    from: `${name} from milandorfling.dev <onboarding@resend.dev>`,
     to: "milandorfling80@gmail.com",
     subject: `New message from ${name}`,
     html: `<p>${message}</p><p>From: ${email}</p>`,
@@ -19,12 +24,4 @@ export async function POST(req) {
   return NextResponse.json(data);
 }
 
-// ~~ Page routing logic for Next.js API route ~~
-
-export const config = {
-  api: {
-    bodyParser: {
-      sizeLimit: "1mb", // Limit request body size to 1MB
-    },
-  },
-};
+export const maxRequestBodySize = "1mb";
